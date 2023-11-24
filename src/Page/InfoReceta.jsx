1ef2b211527c0b677e93c990150979bd0a2d69e2
Andrea Cifuentes <cifuentes17andrea@gmail.com>
2023-11-23 23:30:27 -0500

    Pagina de info de recetas empezada

--- src/Page/InfoReceta.jsx
+++ src/Page/InfoReceta.jsx
@@ -1,21 +1,55 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 const Inforeceta= () => {
-  return (
-    <div
-        className="flex justify-center items-center h-screen"
-       >
-      
-    <p>No séé</p>
-    
+  const navigate = useNavigate();
 
+  const goToInicio = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="relative w-full mx-auto top-0 py-8 sm:py-6 z-10 h-screen">
+     <div className="absolute inset-0 bg-gradient-to-r from-blue-200 to-green-300 shadow-md" style={{ zIndex: -1 }}>   
+     <div className="absolute inset-0 bg-white"
+          style={{
+          margin: '2%',
+          padding: '10px', 
+          backgroundColor:'white'
+        }}
+      >
+
+     <button
+        className="absolute top-4 left-4 bg-green-500 text-white p-6 shadow-lg rounded-full hover:bg-green-600 hover:scale-105 transition duration-300 ease-in-out"
+        onClick={goToInicio}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+          />
+        </svg>
+      </button>
+     <h1 className='text-center italic text-black text-5xl font-bold z-10 bottom-4 left-4'>Tipo de receta</h1>  {/*Aqui va el titulo dependiendo del tipo */}
         
-    </div>
         
         
-      
         
-     
+        
+        
+        
+        
+        </div>
+     </div>  
+   </div>      
   );
 }
 
